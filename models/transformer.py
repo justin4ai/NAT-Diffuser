ae365e553810c0ad7b86f60e4cf240ba61f2eb18
justin4ai <hewas321@gmail.com>
2024-06-07 14:23:01 +0900

    1) Hydra1DNA module updated 2) Rotary embedding applied

--- models/transformer.py
+++ models/transformer.py
@@ -5,15 +5,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.init import trunc_normal_
 import warnings
-from natten.functional import natten2dqkrpb, natten2dav
-# from natten import (
-#       #enable_fused_na,
-#       disable_fused_na,
-#       #enable_autotuner,
-#       disable_autotuner
-# )
-# enable_fused_na()
-# enable_autotuner()
+import sys
+from rotary_embedding_torch import RotaryEmbedding
+from natten.functional import na2d_av, na2d_qk, na1d_av, na1d_qk, na1d
+from natten import (
+    use_fused_na,
+    use_autotuner
+)
+use_fused_na(True)
+use_autotuner(True)
 
 
 class CausalSelfAttention(nn.Module):
@@ -74,107 +74,132 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_drop(self.proj(y)) # torch.Size([20, 256, 512])
         return y, present
     
-class HydraNeighborhoodAttention(nn.Module):
-    def __init__(self,
-                 dim,
-                 kernel_sizes, # Array for kernel sizes
-                 num_heads,
-                 qkv_bias=True,
-                 qk_scale=None,
-                 attn_drop=0.,
-                 proj_drop=0.,
-                 dilations=[1], # Array of dilations
-                 ):
+class Hydra1DNeighborhoodAttention(nn.Module):
+    def __init__(self, H):
         super().__init__()
-        if len(kernel_sizes) == 1 and len(dilations) != 1:
-            kernel_sizes = [kernel_sizes[0] for _ in range(len(dilations))]
-        elif len(dilations) == 1 and len(kernel_sizes) != 1:
-            dilations = [dilations[0] for _ in range(len(kernel_sizes))]
-        assert(len(kernel_sizes) == len(dilations)),f"Number of kernels ({(kernel_sizes)}) must be the same as number of dilations ({(dilations)})"
-        self.num_splits = len(kernel_sizes)
-        self.num_heads = num_heads
-        self.kernel_sizes = kernel_sizes
-        self.dilations = dilations
+        assert H.bert_n_emb % H.bert_n_head == 0
+        # key, query, value projections for all heads
+        self.key = nn.Linear(H.bert_n_emb, H.bert_n_emb)
+        self.query = nn.Linear(H.bert_n_emb, H.bert_n_emb)
+        self.value = nn.Linear(H.bert_n_emb, H.bert_n_emb)
+        # regularization
+        self.attn_drop = nn.Dropout(H.attn_pdrop)
+        self.resid_drop = nn.Dropout(H.resid_pdrop)
+        # output projection
+        self.proj = nn.Linear(H.bert_n_emb, H.bert_n_emb)
+        self.n_head = H.bert_n_head
+        self.causal = True if H.sampler == 'autoregressive' else False
 
-        self.head_dim = dim // self.num_heads
-        self.scale = qk_scale or self.head_dim ** -0.5
+        self.dilations = [1, 4, 8, 16] # Ensure each element must be a divisor of self.n_heads
+        self.kernel_sizes = [7] * len(self.dilations)
+        self.num_splits = len(self.kernel_sizes)
 
-        asserts = []
-        for i in range(len(kernel_sizes)):
-            asserts.append(kernel_sizes[i] > 1 and kernel_sizes[i] % 2 == 1)
-            if asserts[i] == False:
-                warnings.warn(f"Kernel_size {kernel_sizes[i]} needs to be >1 and odd")
-        assert(all(asserts)),f"Kernel sizes must be >1 AND odd. Got {kernel_sizes}"
 
-        self.window_size = []
-        for i in range(len(dilations)):
-            self.window_size.append(self.kernel_sizes[i] * self.dilations[i])
+        self.use_rpb = False
 
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        # Needs to be fixed if we want uneven head splits. // is floored
-        # division
-        if num_heads % len(kernel_sizes) == 0:
-            self.rpb = nn.ParameterList([nn.Parameter(torch.zeros(num_heads//self.num_splits, (2*k-1), (2*k-1))) for k in kernel_sizes])
+        if self.causal:
+            block_size = np.prod(H.latent_shape)
+            mask = torch.tril(torch.ones(block_size, block_size))
+            self.register_buffer("mask", mask.view(1, 1, block_size, block_size))
+
+
+        if self.n_head % len(self.kernel_sizes) == 0:
             self.clean_partition = True
         else:
-            diff = num_heads - self.num_splits * (num_heads // self.num_splits)
-            rpb = [nn.Parameter(torch.zeros(num_heads//self.num_splits, (2*k-1), (2*k-1))) for k in kernel_sizes[:-diff]]
-            for k in kernel_sizes[-diff:]:
-                rpb.append(nn.Parameter(torch.zeros(num_heads//self.num_splits + 1, (2*k-1), (2*k-1))))
-            assert(sum(r.shape[0] for r in rpb) == num_heads),f"Got {sum(r.shape[0] for r in rpb)} heads."
-            self.rpb = nn.ParameterList(rpb)
+            print("Error: Adaptive handling of discrepancy between n_heads and the number of kernel sizes has not been implemented yet.")
+            sys.exit(1) 
+            
+        
+        self.rpb = nn.ParameterList([nn.Parameter(torch.zeros(self.n_head//self.num_splits + 1, (2*k-1), (2*k-1))) for k in self.kernel_sizes])
+        self.rotary_emb = RotaryEmbedding(dim = 32)
 
-            self.clean_partition = False
-            self.shapes = [r.shape[0] for r in rpb]
-            warnings.warn(f"Number of partitions ({self.num_splits}) do not "\
-                    f"evenly divide the number of heads ({self.num_heads}). "\
-                    f"We evenly divide the remainder between the last {diff} "\
-                    f"heads This may cause unexpected behavior. Your head " \
-                    f"partitions look like {self.shapes}")
+    def forward(self, x, layer_past=None):
 
-        [trunc_normal_(rpb, std=0.02, mean=0.0, a=-2., b=2.) for rpb in self.rpb]
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
+        B, T, C = x.size() #torch.Size([20, 256, 512])
 
-    def forward(self, x):
-        B, Hp, Wp, C = x.shape
-        H, W = Hp, Wp
-        qkv = self.qkv(x).reshape(B, H, W, 3, self.num_heads, self.head_dim).permute(3, 0, 4, 1, 2, 5)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs) # torch.Size([20, 8, 256, 64])
+        q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
 
-        q, k, v = qkv.chunk(3, dim=0)
-        q = q.squeeze(0) * self.scale
-        k = k.squeeze(0)
-        v = v.squeeze(0)
+        B, nh, T, hs = q.size()
 
-        if self.clean_partition:
-            q = q.chunk(self.num_splits, dim=1)
+        #########################
+        ## - sync 1 finishes - ##
+        #########################
+
+        present = torch.stack((k, v)) # torch.Size([2, 20, 8, 256, 64]) / not used in our code
+        if self.causal and layer_past is not None:
+            print("?")
+            past_key, past_value = layer_past
+            k = torch.cat((past_key, k), dim=-2)
+            v = torch.cat((past_value, v), dim=-2)
+
+
+        if self.clean_partition: # Always true for this implementation
+
+            #print(f"num_splits : {self.num_splits}") # with low-resolution, 1 / high-resolutions : 2
+            q = q.chunk(self.num_splits, dim=1) # returns self.num_splits of tuple splitted in num_heads -> different multihead groups!
             k = k.chunk(self.num_splits, dim=1)
             v = v.chunk(self.num_splits, dim=1)
+
+        if self.use_rpb:
+            attention = [na1d_qk(_q, _k,
+                        kernel_size=_kernel_size,
+                        dilation=_dilation,
+                        rpb=_rpb) \
+                for _q,_k,_rpb,_kernel_size,_dilation in zip(q, k, self.rpb, self.kernel_sizes, self.dilations)]
         else:
-            i = 0
-            _q = []
-            _k = []
-            _v = []
-            for h in self.shapes:
-                _q.append(q[:, i:i+h, :, :])
-                _k.append(k[:, i:i+h, :, :])
-                _v.append(v[:, i:i+h, :, :])
-                i = i+h
-            q, k, v = _q, _k, _v
+
+            q = self.rotary_emb.rotate_queries_or_keys(q)
+            k = self.rotary_emb.rotate_queries_or_keys(k)
+            v = self.rotary_emb.rotate_queries_or_keys(v)
+
+            attention = [na1d(_q, _k, _v, kernel_size = _kernel_size, dilation = _dilation) \
+                         for _q, _k, _v, _kernel_size, _dilation in zip(q, k, v, self.kernel_sizes, self.dilations) ]
 
 
-        attention = [natten2dqkrpb(_q, _k, _rpb, _kernel_size, _dilation) \
-                     for _q,_k,_rpb,_kernel_size,_dilation in zip(q, k, self.rpb, self.kernel_sizes, self.dilations)]
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        #att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # torch.Size([20, 8, 256, 256])
+        #B, nh, T, hs = q.size()
+
+
+        # print(q[0].size())
+        # print(k[0].size())
+        # print(self.rpb[0].size())
+        # print(self.kernel_sizes[0])
+        # print(self.dilations[0])
+
+        #attention = [na1d_qk(_q.permute(0, 2 ,1, 3), _k.permute(0, 2 ,1, 3),
+
+
         attention = [a.softmax(dim=-1) for a in attention]
         attention = [self.attn_drop(a) for a in attention]
 
-        x = [natten2dav(_attn, _v, _k, _d) \
+        x = [na2d_av(_attn, _v,
+                     kernel_size=_k,
+                     dilation=_d) \
              for _attn, _v, _k, _d in zip(attention, v, self.kernel_sizes, self.dilations)]
 
-        x = torch.cat(x, dim=1)
-        x = x.permute(0, 2, 3, 1, 4).reshape(B, H, W, C)
-        return self.proj_drop(self.proj(x))
+        y = torch.cat(x, dim=1)
+
+        if self.causal and layer_past is None: # not used in this implementation
+            print("inf!!")
+            att = att.masked_fill(self.mask[:, :, :T, :T] == 0, float('-inf'))
+
+
+
+        # att = F.softmax(att, dim=-1) # torch.Size([20, 8, 256, 256])
+        # att = self.attn_drop(att) # torch.Size([20, 8, 256, 256])
+        #y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # re-assemble all head outputs side by side
+        print(f"y shape : {y.size()}")
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+        # output projection
+        y = self.resid_drop(self.proj(y)) # torch.Size([20, 256, 512])
+        return y, present
 
 class Block(nn.Module):
     """ an unassuming Transformer block """
@@ -183,7 +208,7 @@ class Block(nn.Module):
         super().__init__()
         self.ln1 = nn.LayerNorm(H.bert_n_emb)
         self.ln2 = nn.LayerNorm(H.bert_n_emb)
-        self.attn = CausalSelfAttention(H)
+        #self.attn = CausalSelfAttention(H)
         
         h_params = H.items()
         # Save parameters of H
@@ -191,7 +216,7 @@ class Block(nn.Module):
             for key, value in h_params:
                 file.write(f"{key}: {value}\n")
 
-        #self.attn = HydraNeighborhoodAttention(H)
+        self.attn = Hydra1DNeighborhoodAttention(H)
         self.mlp = nn.Sequential(
             nn.Linear(H.bert_n_emb, 4 * H.bert_n_emb),
             nn.GELU(),  # nice
